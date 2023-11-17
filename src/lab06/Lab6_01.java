6c396de80c2962c966b0cfb49f29ec3ecf180977
Phuongna <anhphuongnguyen03@gmail.com>
2023-11-17 17:07:37 +0700

    edit lab6_01

--- src/lab06/Lab6_01.java
+++ src/lab06/Lab6_01.java
@@ -1,6 +1,10 @@
 package lab06;
 
 public class Lab6_01 {
+    /*
+    * Given input string: "2hrs and 5 minutes"
+    * Please calculate how many minutes in total
+     */
 
     public static void main(String[] args) {
         String inputString = "2hour and 5 minutes";
